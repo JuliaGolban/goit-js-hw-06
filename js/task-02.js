3d39298eb10ed6e5ac17afb4018a47dd045cfb10
JuliaGolban <julia.j.shcherban@gmail.com>
2022-07-25 16:26:29 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('ul#ingredients');
+
+// ==== oldSchool:
+// for (let i = 0; i < ingredients.length; i += 1) {
+
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = ingredients[i];
+//   itemEl.classList.add('item');
+//   listIngredients.append(itemEl);
+
+//   console.log(itemEl);
+// }
+
+// ==== modern:
+const elements = ingredients.map(item => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = item;
+  itemEl.classList.add('item');
+  listIngredients.append(itemEl);
+
+  console.log(itemEl);
+});
