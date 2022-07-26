f792840d9cda1a82dd785f287814790c0c58694b
JuliaGolban <julia.j.shcherban@gmail.com>
2022-07-26 14:24:38 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector('#name-input');
+const nameEl = document.querySelector('#name-output');
+console.log(document.querySelector('#name-output'));
+
+inputEl.addEventListener('input', onInputChange);
+function onInputChange(event) {
+  event.currentTarget.value !== ''
+    ? (nameEl.textContent = event.currentTarget.value)
+    : (nameEl.textContent = 'Anonymous');
+}
