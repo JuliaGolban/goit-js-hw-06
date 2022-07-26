5f7013c832c84367bd22f4dad9f08639db3fc440
JuliaGolban <julia.j.shcherban@gmail.com>
2022-07-26 18:29:39 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -4,12 +4,28 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
+
   const {
     elements: { email, password },
   } = event.currentTarget;
   if (email.value === '' || password.value === '') {
     return alert('Please fill in all the fields!');
   }
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  const formData = {
+    email: event.currentTarget.email.value,
+    password: event.currentTarget.password.value,
+  };
+  console.log(formData);
+
+  // *** или так ***
+  // console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  // *** или так ***
+  // const formData = new FormData(event.currentTarget);
+  // formData.forEach((value, name) => {
+  //   console.log(name, value);
+  // });
+
   event.currentTarget.reset();
 }
