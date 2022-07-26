d72936ed8500eec986f03f05cf67cb5aeda2df0a
JuliaGolban <julia.j.shcherban@gmail.com>
2022-07-26 18:50:04 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const bgcName = document.querySelector('.color');
+
+console.dir(bodyEl);
+console.dir(buttonEl);
+console.dir(bgcName);
+
+buttonEl.addEventListener('click', e => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  bgcName.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+console.log(getRandomHexColor());
