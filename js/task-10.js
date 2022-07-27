2166741dc3e6502903f241478115ce47e8e5f2c9
JuliaGolban <julia.j.shcherban@gmail.com>
2022-07-27 11:01:12 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,49 @@
+const inputNumber = document.querySelector('input[type="number"]');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('div#boxes');
+
+console.dir(inputNumber);
+console.dir(btnCreate, btnDestroy);
+console.dir(boxes);
+
+btnCreate.addEventListener('click', getAmount);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function getAmount() {
+  let amount = +inputNumber.value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let baseboxSize = 30;
+  let array = [];
+  for (let i = 0; i < amount; i += 1) {
+    let newBoxes = document.createElement('div');
+    newBoxes.append(i);
+    array.push(newBoxes);
+
+    baseboxSize += 10;
+    newBoxes.style.backgroundColor = getRandomHexColor();
+    newBoxes.style.height = baseboxSize + 'px';
+    newBoxes.style.width = baseboxSize + 'px';
+    newBoxes.style.margin = '10px';
+
+    // let fragment = new DocumentFragment();
+    // for (let i = 0; i < amount; i += 1) {
+    //   const newBoxes = document.createElement('div');
+    //   newBoxes.textContent = +i;
+    //   fragment.appendChild(newBoxes);
+    // }
+    // boxes.append(fragment);
+  }
+  boxes.append(...array);
+}
+
+function destroyBoxes() {
+  boxes.remove();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
